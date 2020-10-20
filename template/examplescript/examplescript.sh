875be6a44364c076293f438de7bfce67ebf720de
Holden Morris <morrisholdenx@gmail.com>
2020-10-20 15:51:04 -0400

    add required code for stackscript & adjust distros

--- template/examplescript/examplescript.sh
+++ template/examplescript/examplescript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##
+
 # <UDF name="ssuser" Label="New user" example="username" />
 # <UDF name="sspassword" Label="New user password" example="Password" />
 # <UDF name="hostname" Label="Hostname" example="examplehost" />
@@ -8,32 +8,45 @@
 # <UDF name="dbuser" Label="MySQL Username" />
 # <UDF name="dbuser_password" Label="MySQL User Password" />
 
-# add sudo user
+## REQUIRED IN EVERY MARKETPLACE SUBMISSION
+# Add Logging to /var/log/stackscript.log for future troubleshooting
+exec 1> >(tee -a "/var/log/stackscript.log") 2>&1
+
+# System Updates updates
+apt-get -o Acquire::ForceIPv4=true update -y
+DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" install grub-pc
+apt-get -o Acquire::ForceIPv4=true update -y
+## END OF REQUIRED CODE FOR MARKETPLACE SUBMISSION
+
+# Add sudo user
 adduser $SSUSER --disabled-password --gecos "" && \
 echo "$SSUSER:$SSPASSWORD" | chpasswd
 adduser $SSUSER sudo
 
-
-# updates
-apt-get -o Acquire::ForceIPv4=true update -y
-DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" install grub-pc
-apt-get -o Acquire::ForceIPv4=true update -y
-
-# set hostname
+# Set hostname
+IP=`hostname -I | awk '{print$1}'`
+HOSTNAME=`dnsdomainname -A`
 hostnamectl set-hostname $HOSTNAME
-echo "127.0.0.1   $HOSTNAME" >> /etc/hosts
+echo $IP $HOSTNAME  >> /etc/hosts
 
-# install apache
+# Install and configure Fail2ban
+apt-get install fail2ban -y
+cd /etc/fail2ban
+cp fail2ban.conf fail2ban.local
+cp jail.conf jail.local
+systemctl start fail2ban
+systemctl enable fail2ban
+
+# Install apache
 apt-get install -y apache2
 
-# edit apache config
+# Edit apache config
 sed -ie "s/KeepAlive Off/KeepAlive On/g" /etc/apache2/apache2.conf
 
-
-# create a copy of the default Apache configuration file for your site
+# Create a copy of the default Apache configuration file for your site
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$WEBSITE.conf
 
-# configuration of vhost file
+# Configuration of vhost file
 cat <<END >/etc/apache2/sites-available/$WEBSITE.conf
 <Directory /var/www/html/$WEBSITE/public_html>
     Require all granted
@@ -48,48 +61,77 @@ cat <<END >/etc/apache2/sites-available/$WEBSITE.conf
 </VirtualHost>
 END
 
-# make public_html & logs
+# Make public_html & logs
 mkdir -p /var/www/html/$WEBSITE/{public_html,logs}
 
-# remove default apache page
+# Remove default apache page
 rm /var/www/html/index.html
 
-# install wordpress
+# Install wordpress
 cd /var/www/html/$WEBSITE/public_html
 wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
 tar -xzf /tmp/wordpress.tar.gz --strip-components=1
 chown -R www-data:www-data /var/www/html/$WEBSITE/
-# remove tar file
+
+# Eemove tar file
 rm /tmp/wordpress.tar.gz
 
-
-# link your virtual host file from the sites-available directory to the sites-enabled directory
+# Link your virtual host file from the sites-available directory to the sites-enabled directory
 a2ensite $WEBSITE.conf
 
-
-# disable the default virtual host to minimize security risks
+# Disable the default virtual host to minimize security risks
 a2dissite 000-default.conf
 
-
-# restart apache
+# Restart apache
 systemctl restart apache2
 
-
-# install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
 echo "mysql-server mysql-server/root_password password $DB_PASSWORD" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $DB_PASSWORD" | debconf-set-selections
 apt-get install -y mysql-server
 
+# Installs expect, runs mysql_secure_installation and runs mysql secure installation.
+apt-get install -y expect
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for ):\"
+send \"$DB_PASSWORD\r\"
+expect \"Change the root password?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+echo "$SECURE_MYSQL"
+# Add Wordpress user & database
 mysql -uroot -p$DB_PASSWORD -e "CREATE DATABASE wordpress;"
 mysql -uroot -p$DB_PASSWORD -e "CREATE USER '$DBUSER' IDENTIFIED BY '$DBUSER_PASSWORD';"
 mysql -uroot -p$DB_PASSWORD -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$DBUSER';"
 
+# Restart MySQL
 systemctl restart mysql
 
-# installing php
-apt-get install -y php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql php-gd
+# Installing PHP
+apt-get install -y php php-pear libapache2-mod-php php-mysql php-gd
 
-# making directory for php? giving apache permissions to that log? restarting php
+# Making directory for php | Giving apache permissions | Restarting PHP
 mkdir /var/log/php
 chown www-data /var/log/php
 systemctl restart apache2
+  
+# Force IPv4 and noninteractive upgrade after script runs to prevent breaking nf_conntrack for UFW
+echo 'Acquire::ForceIPv4 "true";' > /etc/apt/apt.conf.d/99force-ipv4
+export DEBIAN_FRONTEND=noninteractive 
+apt-get upgrade -y
+
+rm /root/StackScript
+rm /root/ssinclude*
+echo "Installation complete!"
+
