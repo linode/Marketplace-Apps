68bba98269181e7986f9da7b592ebc9d19643cab
Cold-Egg <eleu@litespeedtech.com>
2020-10-29 14:21:27 +0800

    rm trash

--- openlitespeed-wordpress/cyberpanel-stackscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### linode
-### Install OpenLiteSpeed and WordPress
-bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Setup/wpimgsetup.sh )
-### Regenerate password for Web Admin, Database, setup Welcome Message
-bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh )
-### Clean up ls tmp folder
-sudo rm -rf /tmp/lshttpd/*
