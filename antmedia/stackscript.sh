84a0d98daf0c317a0cc65e173c8ad4c870025282
muratugureminoglu <murat@murat.ws>
2021-03-10 19:21:32 +0300

    Update stackscript.sh

--- antmedia/stackscript.sh
+++ antmedia/stackscript.sh
@@ -3,7 +3,7 @@
 ZIP_FILE="https://github.com/ant-media/Ant-Media-Server/releases/download/ams-v2.3.0/ant-media-server-2.3.0-community-2.3.0-20210301_0825.zip"
 INSTALL_SCRIPT="https://raw.githubusercontent.com/ant-media/Scripts/master/install_ant-media-server.sh"
 
-wget -q --no-check-certificate $ZIP_FILE -P /tmp/ && wget -q --no-check-certificate $INSTALL_SCRIPT -P /tmp/
+wget -q --no-check-certificate $ZIP_FILE -O /tmp/antmedia.zip && wget -q --no-check-certificate $INSTALL_SCRIPT -P /tmp/
 
 if [ $? == "0" ]; then
   bash /tmp/install_ant-media-server.sh -i /tmp/antmedia.zip
