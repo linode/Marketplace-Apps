8acbd850b0f51f5cf5750f43af3c3afb64aea1bc
Nanda N <nanda.kumar.office1@gmail.com>
2021-06-24 14:11:07 +0530

    Initial NirvaShare artifacts for marketplace

---
+++ nirvashare/nirvashare-stackscript.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# <UDF name="dbpassword" Label="Database Password" />
+
+
+
+# stack script
+
+sudo apt update
+# docker
+
+sudo apt install -yq apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+
+apt-cache policy docker-ce
+
+sudo apt install -yq docker-ce
+
+# docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+# NirvaShare installation
+
+mkdir -p /var/nirvashare
+
+
+echo "version: '3'
+services:
+  admin:
+    image: nirvato/nirvashare-admin:latest
+    container_name: nirvashare_admin
+    networks:
+      - nirvashare
+    restart: always
+    ports:
+#      # Public HTTP Port:
+      - 80:8080
+    environment:
+      ns_db_jdbc_url: 'jdbc:postgresql://nirvashare_database:5432/postgres'
+      ns_db_username: 'nirvashare'
+      ns_db_password: '$DBPASSWORD'
+     
+    depends_on:
+      - db
+
+
+  userapp:
+    image: nirvato/nirvashare-userapp:latest
+    container_name: nirvashare_userapp
+    networks:
+      - nirvashare
+    restart: always
+    ports:
+#      # Public HTTP Port:
+      - 81:8080
+    environment:
+      ns_db_jdbc_url: 'jdbc:postgresql://nirvashare_database:5432/postgres'
+      ns_db_username: 'nirvashare'
+      ns_db_password: '$DBPASSWORD'
+      
+    depends_on:
+      - admin
+
+  db:
+   image: postgres:latest
+   networks:
+      - nirvashare
+   container_name: nirvashare_database
+   restart: always
+#   ports:
+#        - 5432:5432
+   environment: 
+     POSTGRES_PASSWORD: '$DBPASSWORD'
+     POSTGRES_USER: 'nirvashare'
+
+networks:
+  nirvashare: {}
+"  > /var/nirvashare/install-app.yml
+
+
+docker-compose -f /var/nirvashare/install-app.yml up -d
+
+rm /var/nirvashare/install-app.yml
+
+
+
---
+++ nirvashare/nirvashare.md
@@ -0,0 +1,50 @@
+## App Information
+
+### App Name
+
+NirvaShare
+
+### App description
+Securely share and collaborate Linode S3 object storage files/folders with your internal or external users such as customers, partners, vendors, etc with fine access control and in a very simplified manner.
+While granting access to users, set authentications such as Basic, Form, OTP and SAML along with other security restrictions.
+Easily integrates with multiple many external identity providers such as Active Directory, GSuite, AWS SSO, KeyClock, etc
+The FREE version supports Basic, Form and OTP authentication along with many good features.
+
+
+### Version Number
+
+1.3
+
+### Support URLs
+
+* [Contact Form](https://nirvashare.com/contact-us/)
+* [Email](mailto:support@nirvashare.com)
+
+### Operating System
+
+* Ubuntu 20.10 LTS 
+
+### Documentation
+
+Getting started after deploying NirvaShare
+
+After NirvaShare has finished installing, you can access the app instance by visiting your Linode’s IP address(for example, http://192.0.12.100) in a web browser. 
+Please refer documentation at https://nirvashare.com/installation-of-nirvashare-on-linode
+
+## App Assets
+
+### Brand color 1 (HEX code)
+
+9C001B
+
+### Brand color 2 (HEX code) (If left blank, Linode will substitute the secondary brand color with black)
+
+F4AC45
+
+### Logo
+
+* [NirvaShare Logo Colored](assets/logo-nirvashare.svg)
+* [NirvaShare Logo White](assets/logo-nirvashare-white.svg)
+
+Note that the logos are also contained in the [assets.zip](assets.zip) file.
+
