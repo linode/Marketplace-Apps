5b63035bf85003c6c8471ac08bcff8700a2c8edc
Cold-Egg <eleu@litespeedtech.com>
2021-08-03 11:14:56 +0800

    OpenLiteSpeed NodeJS

---
+++ nodejs/openlitespeednodejs-stackscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+### linode
+### Install OpenLiteSpeed and NodeJS
+bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Setup/nodejssetup.sh )
+### Regenerate password for Web Admin, Database, setup Welcome Message
+bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh )
