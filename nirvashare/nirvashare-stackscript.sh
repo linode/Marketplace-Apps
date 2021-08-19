896361dd948ac27d7f93bb0e8c1caf2714cf3d4f
Nanda N <nanda.kumar.office1@gmail.com>
2021-08-19 20:37:38 +0530

    Fixed script to address NirvaShare v1.5.1 capabilities

--- nirvashare/nirvashare-stackscript.sh
+++ nirvashare/nirvashare-stackscript.sh
@@ -3,14 +3,12 @@
 # Script to install NirvaShare applications on Linode
 # Installs docker, docker-compose, postgres db, nirvashare admin and user share app
 #
-#
 # <UDF name="dbpassword" Label="Database Password" />
-
-
-
 # stack script
 
+
 sudo apt update
+
 # docker
 
 sudo apt install -yq apt-transport-https ca-certificates curl software-properties-common
@@ -27,7 +25,7 @@ sudo apt install -yq docker-ce
 
 # docker-compose
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
@@ -35,8 +33,8 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 mkdir -p /var/nirvashare
 
-
 echo "version: '3'
+
 services:
   admin:
     image: nirvato/nirvashare-admin:latest
@@ -51,10 +49,11 @@ services:
       ns_db_jdbc_url: 'jdbc:postgresql://nirvashare_database:5432/postgres'
       ns_db_username: 'nirvashare'
       ns_db_password: '$DBPASSWORD'
-     
+
+    volumes:
+      - /var/nirvashare:/var/nirvashare     
     depends_on:
       - db
-
 
   userapp:
     image: nirvato/nirvashare-userapp:latest
@@ -69,30 +68,35 @@ services:
       ns_db_jdbc_url: 'jdbc:postgresql://nirvashare_database:5432/postgres'
       ns_db_username: 'nirvashare'
       ns_db_password: '$DBPASSWORD'
-      
+    volumes:
+      - /var/nirvashare:/var/nirvashare      
     depends_on:
       - admin
 
   db:
-   image: postgres:latest
+   image: postgres:13.2
    networks:
       - nirvashare
    container_name: nirvashare_database
    restart: always
 #   ports:
+
 #        - 5432:5432
+
    environment: 
      POSTGRES_PASSWORD: '$DBPASSWORD'
      POSTGRES_USER: 'nirvashare'
+   volumes:
+      - db_data:/var/lib/postgresql/data
+
+volumes:
+   db_data:
 
 networks:
   nirvashare: {}
+
 "  > /var/nirvashare/install-app.yml
 
-
 docker-compose -f /var/nirvashare/install-app.yml up -d
-
-#rm /var/nirvashare/install-app.yml
-
 
 
