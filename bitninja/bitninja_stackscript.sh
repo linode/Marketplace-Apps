0efb5406c8cf5cd26d92594679c1d36c14c362ea
SzaboZoli <zoltan.szabo@bitninja.io>
2022-01-06 09:42:00 +0100

    install command modify

--- bitninja/bitninja_stackscript.sh
+++ bitninja/bitninja_stackscript.sh
@@ -2,4 +2,4 @@
 
 # <UDF name="license_key" label="License Key" />
 
-wget -qO- https://get.bitninja.io/install.sh | /bin/bash -s - --license_key="$license_key"
+wget -qO- https://get.bitninja.io/install.sh | /bin/bash -s - --license_key="$license_key" -y
