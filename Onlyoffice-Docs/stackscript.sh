a6178df30e025c3eebe988515db993ec208cee1c
danilapog <danil.titarenko@onlyoffice.com>
2022-12-07 16:28:20 +0300

    Update UDF

--- Onlyoffice-Docs/stackscript.sh
+++ Onlyoffice-Docs/stackscript.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-
-#Onlyoffice-docs StackScript UDF variables
-
+# <UDF name="jwt_enabled" Label="Specifies the enabling the JSON Web Token validation by the ONLYOFFICE Docs. Defaults to true" example="true or false" default="true"> 
+# <UDF name="jwt_secret" Label="Defines the secret key to validate the JSON Web Token in the request to the ONLYOFFICE Docs." example="s4gsd9lfs" default="secret">
+# <UDF name="docs_version" Label="Defines version of Onlyoffice-Docs that you want to run. latest by default" example="7.2.0" default="latest">
 
 # Enable Logging to /var/log/stackscript.log for future troubleshooting
 exec 1> >(tee -a "/var/log/stackscript.log") 2>&1
@@ -9,12 +9,12 @@ exec 1> >(tee -a "/var/log/stackscript.log") 2>&1
 # System Updates updates
 apt-get -o Acquire::ForceIPv4=true update -y
 
-#Get docker
+# Get and install docker
 curl -fsSL get.docker.com | sudo sh
 
-# Run Onlyoffice Docker-DocumentServer
+# Run Onlyoffice-Docs with docker
 sudo docker run -i -t -d -p 80:80 \
-	-e JWT_ENABLED=${JWT_STATUS} \
+	-e JWT_ENABLED=${JWT_ENABLED} \
         -e JWT_SECRET=${JWT_SECRET} \
 	-v /app/onlyoffice/DocumentServer/logs:/var/log/onlyoffice  \
 	-v /app/onlyoffice/DocumentServer/data:/var/www/onlyoffice/Data  \
@@ -26,6 +26,8 @@ sudo docker run -i -t -d -p 80:80 \
 
 # Add MOTD 
 cat >/etc/motd <<EOF
+
+Thank you for install
 
  #######  ##     ## ##   ##      ## #######  ####### ####### ##  #######  #######
 ##     ## ###    ## ##    ##    ## ##     ## ##      ##         ##     ## ##
@@ -43,6 +45,5 @@ cat >/etc/motd <<EOF
                      ##    ##  ##     ## ##     ## ##   ##
                      #######    #######   #######   #####
 
-Thank you for choosing! 
-For help and documentation visit: https://helpcenter.onlyoffice.com/installation/docs-index.aspx
+For more informations about the product features please follow: https://www.onlyoffice.com/compare-editions.aspx
 EOF
