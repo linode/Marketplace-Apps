06f2d9413e9a19e820b88db56b53c816a71e5258
danilapog <danil.titarenko@onlyoffice.com>
2022-12-07 17:47:40 +0300

    Remove the ability to determine the version

--- Onlyoffice-Docs/stackscript.sh
+++ Onlyoffice-Docs/stackscript.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # <UDF name="jwt_enabled" Label="Specifies the enabling the JSON Web Token validation by the ONLYOFFICE Docs. Defaults to true" example="true or false" default="true"> 
 # <UDF name="jwt_secret" Label="Defines the secret key to validate the JSON Web Token in the request to the ONLYOFFICE Docs." example="s4gsd9lfs" default="secret">
-# <UDF name="docs_version" Label="Defines version of Onlyoffice-Docs that you want to run. latest by default" example="7.2.0" default="latest">
 
 # Enable Logging to /var/log/stackscript.log for future troubleshooting
 exec 1> >(tee -a "/var/log/stackscript.log") 2>&1
@@ -22,7 +21,7 @@ sudo docker run -i -t -d -p 80:80 \
         -v /app/onlyoffice/DocumentServer/rabbitmq:/var/lib/rabbitmq \
         -v /app/onlyoffice/DocumentServer/redis:/var/lib/redis \
         -v /app/onlyoffice/DocumentServer/db:/var/lib/postgresql \
-	onlyoffice/documentserver:${DOCS_VERSION}
+	onlyoffice/documentserver:latest
 
 # Add MOTD 
 cat >/etc/motd <<EOF
