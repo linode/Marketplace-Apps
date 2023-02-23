0ce1a03eaf5db371729437732f49fcb90b844625
Jay D <naj1n@mail.com>
2023-02-23 11:51:20 +0800

    update illa-builder stackscript script

--- illa-builder/stackscript.sh
+++ illa-builder/stackscript.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#<UDF name="IBIP" Label="ILLA Builder IP" example="Default: 127.0.0.1" default="127.0.0.1" />
-#<UDF name="IBPORT" Label="ILLA Builder Port" example="Default: 80" default="80" />
-#<UDF name="IBAPORT" Label="ILLA Builder API Port" example="Default: 9999" default="9999" />
-#<UDF name="IBWPORT" Label="ILLA Builder Websocket Port" example="Default: 8000" default="8000" />
-#<UDF name="IBPPORT" Label="ILLA Builder PostgreSQL Port" example="Default: 5432" default="5432" />
+#<UDF name="IP" Label="ILLA Builder IP" example="Default: 127.0.0.1" default="127.0.0.1" />
+#<UDF name="PORT" Label="ILLA Builder Port" example="Default: 80" default="80" />
 
 ## Enable logging
 exec > >(tee /dev/ttyS0 /var/log/stackscript.log) 2>&1
@@ -29,10 +26,6 @@ docker -v
 PG_PASS=mysecretpassword
 ILLA_HOME_DIR=/var/lib/illa
 PG_VOLUMN=${ILLA_HOME_DIR}/database/postgresql
-API_SERVER_ADDRESS=$IBIP
-API_SERVER_PORT=$IBAPORT
-WEBSOCKET_SERVER_ADDRESS=$IBIP
-WEBSOCKER_PORT=$IBWPORT
 WSS_ENABLED=false
 ILLA_DEPLOY_MODE='self-host'
 
@@ -49,24 +42,16 @@ docker run -d \
     -e POSTGRES_PASSWORD=$PG_PASS \
     -e GIN_MODE=release \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
-    -e API_SERVER_ADDRESS=$API_SERVER_ADDRESS \
-    -e API_SERVER_PORT=$API_SERVER_PORT \
-    -e WEBSOCKET_SERVER_ADDRESS=$WEBSOCKET_SERVER_ADDRESS \
-    -e WEBSOCKER_PORT=$WEBSOCKER_PORT \
-    -e WSS_ENABLED=$WSS_ENABLED \
     -e ILLA_DEPLOY_MODE=$ILLA_DEPLOY_MODE \
     -v $PG_VOLUMN:/var/lib/postgresql/data \
-    -p $IBPPORT:5432 \
-    -p $IBPORT:80 \
-    -p $WEBSOCKER_PORT:8000 \
-    -p $API_SERVER_PORT:9999 \
+    -p $PORT:80 \
     illasoft/illa-builder:latest
 
 echo "
 ********************************************************************************
 Welcome to ILLA Builder!
 ********************************************************************************
-  # ILLA Builder:      http://$IP:$BBPORT/
+  # ILLA Builder:      http://$IP:$PORT/
   # Website:           https://www.illacloud.com
   # Documentation:     https://www.illacloud.com/docs/about-illa
   # Github:            https://github.com/illacloud
